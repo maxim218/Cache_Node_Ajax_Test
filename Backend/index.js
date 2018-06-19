baf941a5b8af35d9dd9ff0176f1a1cb7cf042ca8
maxim <maxim218maxim218@yandex.ru>
2018-06-19 20:10:44 +0300

    Add splitting to operations

--- Backend/index.js
+++ Backend/index.js
@@ -3,19 +3,22 @@
 let express = require("express");
 let app = express();
 
-let cache = false;
-
 app.use(function(req, res, next) {
-    console.log("==== Work with headers ====");
+    console.log("==== Application Use Block ====");
+    // allow all clients
     res.header("Access-Control-Allow-Origin", "*");
+    // allow headers
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+function setCacheHeaders(res, cache) {
     if(cache === false) {
         res.header("Cache-Control", "no-cache, no-store, must-revalidate");
     } else {
         res.header("Cache-Control", "max-age=120");
     }
-    next();
-});
+}
 
 
 let port = process.env.PORT || 5001;
@@ -27,17 +30,43 @@ console.log("   ");
 console.log("   ");
 
 app.post('/*', (request, response) => {
+    console.log("----------------------------------------");
+    console.log("POST");
+    console.log("Url: " + request.url);
+
     let bigString = "";
     request.on('data', (data) => {
         bigString += data;
     }).on('end', () => {
-        const dataObj = JSON.parse(bigString);
-        const a = parseInt(dataObj.a);
-        const b = parseInt(dataObj.b);
-        const summa = a + b;
-        console.log("Summa (post): " + summa);
+        console.log("Body: " + bigString);
+
+        const operation = request.url.toString().split("/")[1];
+        console.log("Operation: " + operation);
+
+        if(operation === "postsumma") {
+            const dataObj = JSON.parse(bigString);
+            const a = parseInt(dataObj.a);
+            const b = parseInt(dataObj.b);
+            const summa = a + b;
+            console.log("Summa (post): " + summa);
+            response.status(200);
+            response.end(summa.toString());
+            return;
+        }
+
+        if(operation === "postraznost") {
+            const dataObj = JSON.parse(bigString);
+            const a = parseInt(dataObj.a);
+            const b = parseInt(dataObj.b);
+            const raznost = a - b;
+            console.log("Raznost (post): " + raznost);
+            response.status(200);
+            response.end(raznost.toString());
+            return;
+        }
+
         response.status(200);
-        response.end(summa.toString());
+        response.end("OPERATION_NOT_FOUND");
     });
 });
 
@@ -46,22 +75,30 @@ app.get('/*', (request, response) => {
     console.log("GET");
     console.log("Url: " + request.url);
 
-    if(request.url === "/getstylefile") {
+    const operation = request.url.toString().split("/")[1];
+    console.log("Operation: " + operation);
+
+    if(operation === "getstylefile") {
         console.log("Send CSS file");
         response.status(200);
         response.sendfile("myfile.css");
         return;
     }
 
-    const s = request.url.split("?")[1];
-    const arr = s.split("&");
-    let summa = 0;
-    arr.forEach((value) => {
-        const mass = value.split("=");
-        summa += parseInt(mass[1]);
-    });
-    console.log("Summa: " + summa);
-    response.status(200);
-    response.end(summa.toString());
-});
+    if(operation === "numbersumma") {
+        const s = request.url.split("?")[1];
+        const arr = s.split("&");
+        let summa = 0;
+        arr.forEach((value) => {
+            const mass = value.split("=");
+            summa += parseInt(mass[1]);
+        });
+        console.log("Summa: " + summa);
+        response.status(200);
+        response.end(summa.toString());
+        return;
+    }
 
+    response.status(200);
+    response.end("OPERATION_NOT_FOUND");
+});
